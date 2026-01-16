36d62403a4be47d6de35120093d02632340bc44d
Swaraj Sah <swarajsah143@gmail.com>
2026-01-16 10:39:23 +0530

    Max-water area by two pointer

--- Day6/Max-water.cpp
+++ Day6/Max-water.cpp
@@ -2,16 +2,36 @@
 using namespace std;
 int main(){
     int h,w,ans=0,area;
-    int height[]={1,1};
+    int height[]={1,8,6,2,5,4,8,3,7};
     int size=sizeof(height)/sizeof(height[0]);
-    for(int i=0;i<size;i++){
-        for(int j=i+1;j<size;j++){
-            h=min(height[i],height[j]);
-            w=j-i;
-            area=h*w;
-            ans=max(area,ans);
+    // for(int i=0;i<size;i++){
+    //     for(int j=i+1;j<size;j++){
+    //         h=min(height[i],height[j]);
+    //         w=j-i;
+    //         area=h*w;
+    //         ans=max(area,ans);
+    //     }
+    // }
+    // cout<<"The maximum area is "<<ans<<endl;
+
+  
+// By two pointer approach method
+
+    int lef=0;
+    int rig=size-1;
+    while(lef<rig){
+        w=rig-lef;
+        h=min(height[lef],height[rig]);
+        area=h*w;
+        ans=max(ans,area);
+        if(height[lef]<height[rig]){
+            lef++;
         }
+        else{
+            rig--;
+        }
+
     }
-    cout<<"The maximum area is "<<ans<<endl;
+    cout<<"The area is "<<ans<<endl;
     return 0;
 }
