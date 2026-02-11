211099d40e4e1469bc0099bfdf1bc9e7a1f476c0
Swaraj Sah <swarajsah143@gmail.com>
2026-02-11 14:01:26 +0530

    Added SortBy names leetcode

---
+++ Day9/insertionSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+
+    vector<int> nums={2,11,4,1,5,6,3,7};
+    int n=nums.size();
+
+
+
+
+    
+    for(int k=0;k<n;k++){
+        cout<<nums[k]<<" ";
+    }
+    return 0;
+}
