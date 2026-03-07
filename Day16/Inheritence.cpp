e2e05366f30c7e06d65a2aba69282042e553895d
Swaraj Sah <swarajsah143@gmail.com>
2026-03-07 18:30:55 +0530

    Multiple inheritence

--- Day16/Inheritence.cpp
+++ Day16/Inheritence.cpp
@@ -10,11 +10,21 @@ public:
     }
       
 };
-class Student: public Person{ // here parent class "Person" is inherited
+class Gender{
+public:
+
+    string sex;
+    Gender(string sex){
+        this->sex=sex;
+    }
+};
+class Student: public Person,public Gender{
+     // here parent class "Person" and "Gender" is inherited(Multiple inheritence )
 public:
     int age;
     string name;
-    Student(string name,int age,string dept):Person(dept){ // here constructor is inherited
+    Student(string name,int age,string dept,string sex):Person(dept),Gender(sex){ 
+        // here constructor is inherited
         this->name=name;
         this->age=age;
     }
@@ -22,12 +32,13 @@ public:
         cout<<"Name: "<<name<<endl;
         cout<<"Age: "<<age<<endl;
         cout<<"Department: "<<dept<<endl;
+        cout<<"Sex: "<<sex<<endl;
     }
 
 };
 
 int main(){
-    Student s1("Swaraj",12,"CSE");
+    Student s1("Swaraj",12,"CSE","Male");
     s1.getInfo();
     return 0;
 }
